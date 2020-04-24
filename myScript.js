f087bc8101375b1deb5be524758dcab8e65c0c22
AdewaleK <pearlkelar1@gmail.com>
2020-04-24 02:34:26 +0100

    ADDS EXTERNAL JS SCRIPT TO PROJECT FOLDER

---
+++ myScript.js
@@ -0,0 +1,10 @@
+
+    function myFunction() {
+        var links = document.getElementById("mylinks");
+        if (links.style.display === "block") {
+              links.style.display = "none";
+            } 
+              else {
+              links.style.display = "block";
+            }
+      }
